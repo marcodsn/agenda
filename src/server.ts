4fd2b74f9cf7c5d38de3d61ff2c98a75d9626c36
Marco De Santis <marcodsn.bit@gmail.com>
2024-10-11 15:43:23 +0200

    Set up the node environment

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+// Hello world! server.ts
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
